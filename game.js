33d4bfd4dfe05e29ecba3405dde50e4eda6b311a
dzcorovic <dzenana.corovic@evolt.dev>
2022-03-06 23:02:51 +0100

    create a proggress bar

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const question = document.getElementById("question")
 const choices = Array.from(document.getElementsByClassName("choice-text"))
-const questionCounterText = document.getElementById("questionCounter")
+const progressText = document.getElementById("progressText")
+const progressBarFull = document.getElementById("progressBarFull")
 const scoreText = document.getElementById("score")
 
 let currentQuestion = {};
@@ -52,10 +53,13 @@ getNewQuestion = () => {
     if (avaliableQuestion.length == 0 || questionCounter >= MAX_QUESTIONS)
         return window.location.assign('/end.html');
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     const questionIndex = Math.floor(Math.random() * avaliableQuestion.length);
     currentQuestion = avaliableQuestion[questionIndex];
     question.innerText = currentQuestion.question;
+    console.log((questionCounter/MAX_QUESTIONS)*100)
+    console.log(`${(questionCounter/MAX_QUESTIONS) * 100} %`)
+    progressBarFull.style.width = `${( questionCounter / MAX_QUESTIONS ) * 100}%`;
 
     choices.forEach( choice => {
         const number = choice.dataset['number'];
